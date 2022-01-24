ae2ff0602194c547cc77d2d1158355b309fa0b32
unknown <hindhujhasridhar@gmail.com>
2022-01-24 22:32:57 +0530

    LIST IS CREATED

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -6,6 +6,22 @@ namespace Collections
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO COLLECTIONS");
+            Console.WriteLine("CREATING A LIST OF NAMES");
+            List();
+        }
+
+        private  static void List()
+        {
+            List<string> list = new List<string>();
+            list.Add("HINDHUJHA");
+            list.Add("SRIDHAR");
+            list.Add("ANITA");
+            list.Add("RONI");
+
+            foreach(var element in list)
+            {
+                Console.WriteLine(element);
+            }
         }
     }
 }
