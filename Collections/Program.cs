484334259decba456eaef3986ba89fc3fc9a038f
unknown <hindhujhasridhar@gmail.com>
2022-01-24 22:41:57 +0530

    PUSH/POP OPERATIONS PERFORMED IN STACK

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -8,6 +8,9 @@ namespace Collections
             Console.WriteLine("WELCOME TO COLLECTIONS");
             Console.WriteLine("CREATING A LIST OF NAMES");
             List();
+            Console.WriteLine("****************************************");
+            Console.WriteLine("CREATING A STACK OF FRUITS");
+            Stack();
         }
 
         private  static void List()
@@ -19,6 +22,28 @@ namespace Collections
             list.Add("RONI");
 
             foreach(var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        private static void Stack()
+        {
+            Stack<string> stack = new Stack<string>();
+            stack.Push("APPLE");
+            stack.Push("ORANGE");
+            stack.Push("MANGO");
+            stack.Push("PAPAYA");
+
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+            //pop
+            string pop = stack.Pop();
+            Console.WriteLine("****************************************");
+            Console.WriteLine("AFTER DELETING FIRST ELEMENT BY POP");
+            foreach (var element in stack)
             {
                 Console.WriteLine(element);
             }
